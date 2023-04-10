cebfc62311fe562f650aed4913f23e5f8a819071
renzo <renzitofabri@hotmail.com>
2023-04-10 11:23:40 -0500

    feed: add sweetalert

---
+++ js/SWAlert.js
@@ -0,0 +1,9 @@
+function confirmAlert() {
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Ha sido enviado correctamente",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+}
